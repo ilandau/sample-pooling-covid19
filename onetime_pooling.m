9d5d468217e2abdb03c0f91bc1f315ef432ee659
ilandau <45141477+ilandau@users.noreply.github.com>
2020-04-07 01:11:24 +0300

    Update onetime_pooling.m

--- onetime_pooling.m
+++ onetime_pooling.m
@@ -1,42 +1,50 @@
-function [num_tests, b_initial] = onetime_pooling(p,N, num_possible_batch_sizes)
+function [num_tests, b_initial] = onetime_pooling(p, N, max_batch_size, preset_ranges)
 % Uses method of Optimal One-Time Pooling to test N total samples with frequency of positives, p,
 % INPUTS:
 % p - frequency of positives
 % N - total number of samples (will be rounded to allow whole number of batches)
-% num_possible_batch_sizes (OPTIONAL) - defines how many possible batch sizes to choose from
-%           from within pre-determined ranges of values of p
-%     if not set, or set to 0, default is to calculate desired batch size
-%           for each given p
-% Returns fractional number of tests, i.e. number of tests / number of samples
-%     and initial batch size
+% max_batch_size (OPTIONAL) - defines maximal batch size
+% preset_ranges (OPTIONAL) - if true, use predefined ranges of values from published protocol
+%           DEFAULT: preset_ranges = false
+%
+% OUTPUTS:
+% num_tests - number of tests / number of samples
+% b_initial - initial batch size
 
 q=1-p;
 
 % Set number of possible initial batch sizes
 % If set to 0, compute optimal batch size for each p
-if ~exist('num_possible_batch_sizes','var')
-    num_possible_batch_sizes = 0;
+if ~exist('preset_ranges','var')
+    preset_ranges = false;
 end
 
 %% Initial Batch Size
 
 % set to optimal batch size given p
-if num_possible_batch_sizes == 0
-    b_initial = round(fsolve(@(a) 1+a.^2.*log(q).*q.^a, 1));
-    
-% Otherwise use pre-defined ranges of values 
-elseif p < 0.0005 & num_possible_batch_sizes > 5
-    b_initial = 64;
-elseif p < 0.001 & num_possible_batch_sizes > 4
-    b_initial = 32;
-elseif p < 0.003 & num_possible_batch_sizes > 3
-    b_initial = 24;
-elseif p<0.008 & num_possible_batch_sizes > 2
-	b_initial = 16;
-elseif p<0.04 & num_possible_batch_sizes > 1
-	b_initial = 8;
-else
-	b_initial=4;
+b_initial = round(fsolve(@(a) 1+a.^2.*log(q).*q.^a, 1));
+
+% Or use pre-defined ranges of values 
+if preset_ranges
+
+    if p < 0.0005
+        b_initial = 64;
+    elseif p < 0.001
+        b_initial = 32;
+    elseif p < 0.003
+        b_initial = 24;
+    elseif p<0.008
+        b_initial = 16;
+    elseif p<0.04
+        b_initial = 8;
+    else
+        b_initial=4;
+    end
+end
+
+% compare to max batch size
+if exist('max_batch_size','var')
+    b_initial = min(b_initial, max_batch_size);
 end
 
 %% Set up Batches
