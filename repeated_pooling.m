ff54a74b5d01d8073e26476375a880b3a31e6bfe
ilandau <45141477+ilandau@users.noreply.github.com>
2020-04-07 01:12:01 +0300

    Update repeated_pooling.m

--- repeated_pooling.m
+++ repeated_pooling.m
@@ -1,12 +1,15 @@
-function [num_tests, b_initial] = repeated_pooling(p,N, num_possible_batch_sizes)
+function [num_tests, b_initial] = repeated_pooling(p,N, max_batch_size, min_batch_size, round_down)
 % Uses method of Repeated Pooling to test N total samples with frequency of positives, p,
+%
 % INPUTS:
 % p - frequency of positives
 % N - total number of samples (will be rounded to allow whole number of batches)
-% num_possible_batch_sizes (OPTIONAL) - defines how many possible batch sizes to choose from
-%           from within pre-determined ranges of values of p
-%     if not set, or set to 0, default is to calculate desired batch size
-%           for each given p
+% max_batch_size (OPTIONAL) - defines maximal batch size
+% min_batch_size (OPTIONAL) - any positive batch smaller than or equal to min_batch_size will be tested individually
+%       DEFAULT: min_batch_size = 4
+% round_down (OPTIONAL) - if true, round batch sizedown to nearest power of 2
+%       DEFAULT: round_down = true
+%
 % OUTPUTS:
 % num_tests - number of tests / number of samples
 % b_initial - initial batch size
@@ -14,35 +17,36 @@ function [num_tests, b_initial] = repeated_pooling(p,N, num_possible_batch_sizes
 
 q=1-p;
 
-% Set number of possible initial batch sizes
-% If set to 0, compute optimal batch size for each p
-if ~exist('num_possible_batch_sizes','var')
-    num_possible_batch_sizes = 0;
+% round_down batch size to nearest power of 2 or not
+if ~exist('round_down','var')
+    round_down = true;
 end
 
-% min_length defines size of batch that is not to be split
+
+% min_batch_size defines size of batch that is not to be split
 %   but rather to be tested individually
-min_length = 4;
+if ~exist('min_batch_size','var')
+    min_batch_size = 4;
+end
+
 
 
 %% Initial Batch Size
 
-% Set to batch size so that each batch has 0.5 prob of being negative
-if num_possible_batch_sizes == 0 
-    b_initial = round(-log(2)/log(q));
-    
-% Otherwise use pre-defined ranges of values    
-elseif p < 0.007 & num_possible_batch_sizes > 4
-    b_initial = 96;
-elseif p < 0.01 & num_possible_batch_sizes > 3
-    b_initial = 64;
-elseif p < 0.02 & num_possible_batch_sizes > 2
-    b_initial = 32;
-elseif p < 0.003 & num_possible_batch_sizes > 1
-	b_initial=16;
-else
-	b_initial=8;
+% Set to batch size so that each batch has 0.5 prob of being negative,
+b_initial = round(-log(2)/log(q));
+
+
+% round down to nearest power of 2
+if round_down
+    b_initial = 2^max(2,(floor(log2(-log(2)/log(q)))));
 end
+
+% compare to max batch size
+if exist('max_batch_size','var')
+    b_initial = min(b_initial, max_batch_size);
+end
+
 
 %% Set up Batches
 % round total number of samples to fit whole number of batches
@@ -68,7 +72,7 @@ for bi=pos_batches
 	batch = batches(:,bi);
     % split_and_recurse splits a positive batch into, checks both halves,
     % and repeats, returning number of tests accumulated
-	ntests = ntests + split_and_recurse(batch,min_length);	
+	ntests = ntests + split_and_recurse(batch,min_batch_size);	
 end
 
 num_tests = ntests/n;
